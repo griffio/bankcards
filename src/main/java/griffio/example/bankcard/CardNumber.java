7ae3e3eae88fa6ac80d46cf521670fc8b035f658
griffio <griffio@users.noreply.github.com>
2015-01-31 14:48:55 +0000

    implement masked digits
    Signed-off-by: griffio <griffio@users.noreply.github.com>

--- src/main/java/griffio/example/bankcard/CardNumber.java
+++ src/main/java/griffio/example/bankcard/CardNumber.java
@@ -1,18 +1,24 @@
 package griffio.example.bankcard;
 
 import com.google.auto.value.AutoValue;
+
+import static com.google.common.base.CharMatcher.DIGIT;
+
 /**
  * AutoValue (https://github.com/google/auto/tree/master/value)
  * An immutable value type to represent the card number.
  */
 @AutoValue
 public abstract class CardNumber {
-    CardNumber() {}
+
+    CardNumber() {
+    }
 
     public abstract String digits();
 
     public String maskedDigits() {
-      return digits();
+        String fourDigits = digits().substring(0, 4);
+        return fourDigits.concat(DIGIT.replaceFrom(digits().substring(4), 'x'));
     }
 
     public static CardNumber create(String digits) {
